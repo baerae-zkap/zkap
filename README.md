d62210f53601711bc1e765d54efab1655146734e
KaiKim <kim.kyungkoo@gmail.com>
2025-03-10 23:45:24 +0900

    fix bugs

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # zkap-aa-sdk
+
 A TypeScript SDK for interacting with ZKAP Abstract Account contracts easily.
+
+## 빌드 및 배포
+
+### 빌드
+
+```bash
+yarn build
+```
+
+### 퍼블리시
+
+```bash
+yarn pub
+```
+
+## 패키지 설치 방법
+
+```bash
+yarn add @baerae-zkap/zkap-aa-sdk@<version>
+```
+
+## npmrc 파일 작성 방법
+
+프로젝트 루트 디렉토리에 `.npmrc` 파일을 생성하고 다음 내용을 추가합니다:
+
+```ini
+//npm.pkg.github.com/:_authToken=< Your Token >
+@baerae-zkap:registry=https://npm.pkg.github.com/
+```
+
+이 설정은 npm이 패키지를 설치할 때 사용할 기본 레지스트리를 지정합니다.
+
+< Your Token > 부분에 개인 계정의 깃허브 토큰을 넣으면 동작합니다.
+
+### 토큰 발급 위치
+
+> Github / Settings / Developer Settings / Personal access tokens (classic) / New personal access token (classic)
+
+## 사용 방법
--- lib/builders/ZkapBuilder.ts
+++ lib/builders/ZkapBuilder.ts
@@ -13,9 +13,6 @@ import {
 import { IUserOpSigner } from "../utils/IUserOpSigner";
 import { ethers } from "ethers";
 
-// const ERC1967PROXY_CREATION_CODE =
-//   "0x60806040526102a88038038061001481610168565b92833981016040828203126101645781516001600160a01b03811692909190838303610164576020810151906001600160401b03821161016457019281601f8501121561016457835161006e610069826101a1565b610168565b9481865260208601936020838301011161016457815f926020809301865e86010152823b15610152577f360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc80546001600160a01b031916821790557fbc7cd75a20ee27fd9adebab32041f755214dbc6bffa90cc0225b39da2e5c2d3b5f80a282511561013a575f8091610122945190845af43d15610132573d91610113610069846101a1565b9283523d5f602085013e6101bc565b505b604051608d908161021b8239f35b6060916101bc565b50505034156101245763b398979f60e01b5f5260045ffd5b634c9c8ce360e01b5f5260045260245ffd5b5f80fd5b6040519190601f01601f191682016001600160401b0381118382101761018d57604052565b634e487b7160e01b5f52604160045260245ffd5b6001600160401b03811161018d57601f01601f191660200190565b906101e057508051156101d157805190602001fd5b63d6bda27560e01b5f5260045ffd5b81511580610211575b6101f1575090565b639996b31560e01b5f9081526001600160a01b0391909116600452602490fd5b50803b156101e956fe60806040525f8073ffffffffffffffffffffffffffffffffffffffff7f360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc5416368280378136915af43d5f803e156053573d5ff35b3d5ffdfea26469706673582212204d5a42735b382b9656455f8cf6e20d00c3dc42b8251870be6293326a6a634eee64736f6c634300081c0033";
-
 export class ZkapBuilder extends BaseAccountBuilder {
   protected factoryInterface: ethers.Interface = new ethers.Interface(
     ZkapAccountFactoryABIstring
@@ -99,7 +96,7 @@ export class ZkapBuilder extends BaseAccountBuilder {
     );
   }
 
-  async setInitialGasInfo(): Promise<this> {
+  private async setInitialGasInfo(): Promise<this> {
     if (!this.provider) {
       throw new Error("Provider is not set. Please provide a valid RPC URL.");
     }
@@ -114,6 +111,76 @@ export class ZkapBuilder extends BaseAccountBuilder {
     this.userOp.preVerificationGas = ethers.toBeHex("25000"); // preVerificationGas 값은 25000으로 고정
     this.userOp.verificationGasLimit = ethers.toBeHex("0");
 
+    // verification 할 때 필요한 gas 계산 -> 각 키 타입에 따라 필요한 gas 를 사전에 정의한 값으로 설정
+    if (this.userOp.initCode !== "0x" && this.userOp.initCode !== undefined) {
+      const zkapFactory = ethers.dataSlice(this.userOp.initCode, 0, 20);
+      const callData = ethers.dataSlice(this.userOp.initCode, 20);
+      const walletCreationGasLimit = await this.provider.estimateGas({
+        to: zkapFactory,
+        data: callData,
+      });
+
+      this.userOp.verificationGasLimit = BigInt(
+        walletCreationGasLimit
+      ).toString();
+      const [keyType, [_, keys]] = this.getTxKeyInfo();
+      if (Number(keyType) === CompositeAccountKeyTypes.keyMultisig) {
+        for (const key of keys) {
+          const ADDRESS_KEY_VALIDATION_GAS = 400000;
+          const WEB_AUTHN_KEY_VALIDATION_GAS = 400000;
+          const OAUTH_KEY_VALIDATION_GAS = 400000;
+          const SECP256K1_KEY_VALIDATION_GAS = 400000;
+          const SECP256R1_KEY_VALIDATION_GAS = 400000;
+          const keyType = Number(key.keyType);
+          if (keyType === PrimitiveAccountKeyTypes.keyAddress) {
+            this.userOp.verificationGasLimit = ethers.toBeHex(
+              (
+                BigInt(this.userOp.verificationGasLimit) +
+                BigInt(ADDRESS_KEY_VALIDATION_GAS)
+              ).toString()
+            );
+          } else if (keyType === PrimitiveAccountKeyTypes.keyWebAuthn) {
+            this.userOp.verificationGasLimit = ethers.toBeHex(
+              (
+                BigInt(this.userOp.verificationGasLimit) +
+                BigInt(WEB_AUTHN_KEY_VALIDATION_GAS)
+              ).toString()
+            );
+          } else if (keyType === PrimitiveAccountKeyTypes.keyOAuthRS256) {
+            this.userOp.verificationGasLimit = ethers.toBeHex(
+              (
+                BigInt(this.userOp.verificationGasLimit) +
+                BigInt(OAUTH_KEY_VALIDATION_GAS)
+              ).toString()
+            );
+          } else if (keyType === PrimitiveAccountKeyTypes.keySecp256k1) {
+            this.userOp.verificationGasLimit = ethers.toBeHex(
+              (
+                BigInt(this.userOp.verificationGasLimit) +
+                BigInt(SECP256K1_KEY_VALIDATION_GAS)
+              ).toString()
+            );
+          } else if (keyType === PrimitiveAccountKeyTypes.keySecp256r1) {
+            this.userOp.verificationGasLimit = ethers.toBeHex(
+              (
+                BigInt(this.userOp.verificationGasLimit) +
+                BigInt(SECP256R1_KEY_VALIDATION_GAS)
+              ).toString()
+            );
+          }
+        }
+      } else {
+        throw new Error("Invalid key type");
+      }
+    } else {
+      // 지갑이 만들어져있는 상태에서 verification 할 때에는 verificationGasLimit 을 signature 의 길이에 비례하여 대략적으로 초기값을 설정
+      // check signature length && length / 64 * 100000 을 this.userOp.verificationGasLimit 에 할당
+      const signatureLength = this.userOp.signature?.length ?? 0 / 64;
+      this.userOp.verificationGasLimit = ethers.toBeHex(
+        (signatureLength * 100000).toString()
+      );
+    }
+
     // set callGasLimit
     if (
       this.userOp.callData !== "0x" &&
@@ -123,41 +190,6 @@ export class ZkapBuilder extends BaseAccountBuilder {
       // submit(address) : 0xa1903eab
       if (this.userOp.initCode !== "0x" && this.callMethodId === "0xa1903eab") {
         this.userOp.callGasLimit = ethers.toBeHex("110000");
-        // verification 할 때 필요한 gas 계산 -> 각 키 타입에 따라 필요한 gas 를 사전에 정의한 값으로 설정
-        const [keyType, [_, keys]] = this.getTxKeyInfo();
-        if (Number(keyType) === CompositeAccountKeyTypes.keyMultisig) {
-          for (const key of keys) {
-            const ADDRESS_KEY_VALIDATION_GAS = 400000;
-            const WEB_AUTHN_KEY_VALIDATION_GAS = 400000;
-            const OAUTH_KEY_VALIDATION_GAS = 400000;
-            const SECP256K1_KEY_VALIDATION_GAS = 400000;
-            const SECP256R1_KEY_VALIDATION_GAS = 400000;
-            const keyType = Number(key.keyType);
-            if (keyType === PrimitiveAccountKeyTypes.keyAddress) {
-              this.userOp.verificationGasLimit = ethers.toBeHex(
-                BigInt(ADDRESS_KEY_VALIDATION_GAS).toString()
-              );
-            } else if (keyType === PrimitiveAccountKeyTypes.keyWebAuthn) {
-              this.userOp.verificationGasLimit = ethers.toBeHex(
-                BigInt(WEB_AUTHN_KEY_VALIDATION_GAS).toString()
-              );
-            } else if (keyType === PrimitiveAccountKeyTypes.keyOAuthRS256) {
-              this.userOp.verificationGasLimit = ethers.toBeHex(
-                BigInt(OAUTH_KEY_VALIDATION_GAS).toString()
-              );
-            } else if (keyType === PrimitiveAccountKeyTypes.keySecp256k1) {
-              this.userOp.verificationGasLimit = ethers.toBeHex(
-                BigInt(SECP256K1_KEY_VALIDATION_GAS).toString()
-              );
-            } else if (keyType === PrimitiveAccountKeyTypes.keySecp256r1) {
-              this.userOp.verificationGasLimit = ethers.toBeHex(
-                BigInt(SECP256R1_KEY_VALIDATION_GAS).toString()
-              );
-            }
-          }
-        } else {
-          throw new Error("Invalid key type");
-        }
       } else {
         let callGasLimit;
         if (this.isCallFromEntryPoint) {
@@ -176,11 +208,6 @@ export class ZkapBuilder extends BaseAccountBuilder {
           });
         }
         this.userOp.callGasLimit = ethers.toBeHex(callGasLimit);
-        // check signature length && length / 64 * 100000 을 this.userOp.verificationGasLimit 에 할당
-        const signatureLength = this.userOp.signature?.length ?? 0 / 64;
-        this.userOp.verificationGasLimit = ethers.toBeHex(
-          (signatureLength * 100000).toString()
-        );
       }
     } else {
       throw new Error("Call data is not set. Please set a valid call data.");
@@ -189,7 +216,7 @@ export class ZkapBuilder extends BaseAccountBuilder {
     return this;
   }
 
-  async finalizeUserOp(): Promise<this> {
+  private async finalizeUserOp(): Promise<this> {
     // verificationGasLimit 정확한 값으로 업데이트
     if (
       typeof this.userOp.initCode === "string" &&
@@ -217,7 +244,7 @@ export class ZkapBuilder extends BaseAccountBuilder {
           ((BigInt(estimatedGas) * BigInt(120)) / BigInt(100)).toString()
         );
       } catch (error) {
-        console.error("Error estimating gas:", error);
+        throw new Error("Error estimating gas: " + error);
       }
       const newUserOpHash = this.getUserOpHash();
       const signature = await this.userOpSigner.signUserOpHash(newUserOpHash);
