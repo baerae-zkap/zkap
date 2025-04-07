8d985c2744fa70bb73887cca46f088d7aacb31dc
KaiKim <kim.kyungkoo@gmail.com>
2025-04-07 12:37:45 +0900

    add zkapAccountFactory

---
+++ lib/account/ZkapAccountFactory.ts
@@ -0,0 +1,32 @@
+import { ethers } from "ethers";
+import { ZkapAccountFactoryABIstring } from "../resources/abis";
+
+export class ZkapAccountFactory {
+  private provider: ethers.JsonRpcProvider;
+  private accountFactory: ethers.Contract;
+
+  constructor(address: string, enUrl: string) {
+    this.provider = new ethers.JsonRpcProvider(enUrl);
+    this.accountFactory = new ethers.Contract(
+      address,
+      ZkapAccountFactoryABIstring,
+      this.provider
+    );
+  }
+
+  async calcAccountAddress(
+    salt: string,
+    compositeAccountKeyFactoryAddress: string,
+    encodedMasterKey: string,
+    encodedTxKey: string
+  ) {
+    const address = await this.accountFactory.calcAccountAddress(
+      salt,
+      compositeAccountKeyFactoryAddress,
+      encodedMasterKey,
+      encodedTxKey
+    );
+
+    return address;
+  }
+}
