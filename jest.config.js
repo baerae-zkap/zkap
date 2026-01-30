957672cf1d0531d7eecfd32abb28f8d2e92115c3
kaikookim <kyungkookim.kr@gmail.com>
2026-01-31 08:55:40 +0900

    test: temporarily raise coverage thresholds to 96% for CI testing

--- jest.config.js
+++ jest.config.js
@@ -33,10 +33,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      statements: 95,
-      branches: 90,
-      functions: 95,
-      lines: 95,
+      statements: 96,
+      branches: 96,
+      functions: 96,
+      lines: 96,
     },
   },
   transform: {
